9cd545cac902ee499729d23342d3dbb6893096b3
Na11a <60902599+Na11a@users.noreply.github.com>
2024-04-24 15:14:39 +0300

    tested and successfully run

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,30 @@
-import type { Metadata } from "next";
 import { Work_Sans } from "next/font/google";
+
 import "./globals.css";
-import { Room } from "./Room";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
-const work_sans = Work_Sans({
-  subsets: ["latin"],
-  variable: "--font-work-sans",
-  weight: ['400', '600', '700']
- });
+import Room from "./Room";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Figma Clone",
-  description: "A minimalist Figma clone",
+  description:
+    "A minimalist Figma clone using fabric.js and Liveblocks for realtime collaboration",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={`${work_sans.className} bg-primary-grey-200`}>
-        <Room>
-          {children}
-        </Room>
-      </body>
-    </html>
-  );
-}
+const workSans = Work_Sans({
+  subsets: ["latin"],
+  variable: "--font-work-sans",
+  weight: ["400", "600", "700"],
+});
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+  <html lang='en'>
+    <body className={`${workSans.className} bg-primary-grey-200`}>
+      <Room>
+        <TooltipProvider>{children}</TooltipProvider>
+      </Room>
+    </body>
+  </html>
+);
+
+export default RootLayout;
